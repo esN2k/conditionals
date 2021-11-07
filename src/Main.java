059f47115c7cbd2feaf46f0eb57ae5e36fb6ac57
BuildTools <dodikbalaman@hotmail.com>
2021-11-07 16:07:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main                                                       {
+
+    public static void main(String[] args)                              {
+        int sayi = 1;
+
+        if (sayi < 20)                                                  {
+            System.out.println("Verdiğiniz sayı 20'den küçüktür.");
+                                                                        }
+        else if(sayi == 20)                                             {
+            System.out.println("Verdiğiniz sayı 20'ye eşittir.");
+                                                                        }
+        else                                                            {
+            System.out.println("Verdiğiniz sayı 20'den büyüktür.");
+                                                                        }
+                                                                        }
+                                                                        }
